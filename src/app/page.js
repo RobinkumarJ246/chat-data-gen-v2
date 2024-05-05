ce173cee3e1eb6968475f2918921e59948baf670
RobinkumarJ246 <robin246j@gmail.com>
2024-05-05 14:06:25 +0530

    Profile page

--- src/app/page.js
+++ src/app/page.js
@@ -67,7 +67,7 @@ const Home = () => {
             <span className="ml-2">Manage Data</span>
           </div>
           {isLoggedIn && (
-            <Link href="/" className="flex items-center text-white">
+            <Link href="/profile" className="flex items-center text-white">
               <span className="text-2xl">&#9881;</span>
               <span className="ml-2">Profile</span>
             </Link>
