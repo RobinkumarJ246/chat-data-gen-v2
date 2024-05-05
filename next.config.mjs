91bb30bf67b0212609a05a5a4dd33abecb4ea48d
RobinkumarJ246 <robin246j@gmail.com>
2024-05-05 15:49:35 +0530

    Vercel.json CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,4 @@
-// Import necessary modules
-const { HeadersFunction } = require('next');
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// Define the headers function
-const headers = async () => {
-  return [
-    {
-      // matching all API routes
-      source: "/api/:path*",
-      headers: [
-        { key: "Access-Control-Allow-Credentials", value: "true" },
-        { key: "Access-Control-Allow-Origin", value: "*" },
-        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-        { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-      ]
-    }
-  ];
-};
-
-// Define the next config object
-const nextConfig = {
-  async headers() {
-    return headers();
-  }
-};
-
-// Export the next config object
-module.exports = nextConfig;
+export default nextConfig;
